ffb9de21c69568538fa1c98a5ab4d652b8ab9c46
kewlness <greenupm@gmail.com>
2018-09-25 22:05:17 -0500

    Memory addresses

---
+++ 07_memory-address/02_using-address/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+const metersToYards float64 = 1.09361
+
+func main() {
+	var meters float64
+	fmt.Print("Enter meters swam: ")
+	fmt.Scan(meters) // get input and put it in the memory address of the meters variable
+	yards := meters * metersToYards
+	fmt.Println(meters, "meters is", yards, "yards.")
+}
